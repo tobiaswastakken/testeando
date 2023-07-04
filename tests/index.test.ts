c2ddacd0c18406403d8b80708b4ef3851fbb3ac2
Miguel-prog1 <md9914461@gmail.com>
2023-07-04 19:04:55 -0300

    hola

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,5 +1,18 @@
-import HelloWorld from "../src/index";
+import {editarNombre} from "../src/editarContacto";
+import {contactos} from "../src/contactos"
 
-test("HelloWorld deberia decir --Hello World", () => {
-  expect(HelloWorld).toBe("Hello World");
+describe("Editar nombre o apellido de contactos", () => {
+  test("Editar el nombre de un contacto debe ser guardado con el nombre actualizado", () => {
+    const contacto = contactos[0]
+    const respuesta = editarNombre(contacto.id, "Carolina");
+    expect(contacto.first_name).toBe("Carolina")
+  });
+  test("No se puede editar el nombre de un contacto inexistente", () =>{
+   expect(editarNombre(20, "Nahuel")).toThrowError("No existe el contacto")
+
+  })
 });
+
+describe("Editar email de los contactos para actualizarlos")
+
+
