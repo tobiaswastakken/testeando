09bfd156242b49824bf386d0ecb6974347c98da0
Miguel-prog1 <md9914461@gmail.com>
2023-07-04 17:02:06 -0300

    comitear

--- src/findOneByEmail.ts
+++ src/findOneByEmail.ts
@@ -1,10 +1,27 @@
 import { Contacto, contactos } from "../src/contactos";
 
-function findOneByEmail(email: string){
 
-}
+
+function findOneByEmail(email:string){
+    const contactoVacio :Contacto = {
+        id: 0,
+        first_name: "",
+        last_name: "",
+        email: "",
+        gender: "",
+        ip_address: "",
+    }
+
+    for (const contacto of contactos) {
+        if (contacto.email == email) {
+            return contacto;
+        }
+    }
+    return contactoVacio
+};
+
+
+
+
 
 export { findOneByEmail };
-
-
-//dile q tu me quiereee 
